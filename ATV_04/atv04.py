c1fa71ccdb11b3cc5929efb5641075244599f2e8
Miguel Gut Seara <miguelgseara@gmail.com>
2021-05-25 22:03:24 -0300

    Atividade 04 - Publicando dados de API.

--- ATV_04/atv04.py
+++ ATV_04/atv04.py
@@ -2,6 +2,7 @@
 from pyowm import OWM
 from pyowm.utils import config
 from pyowm.utils import timestamps
+import config
 import time
 import json
 import paho.mqtt.client as paho
@@ -10,27 +11,24 @@ import random
 
 warnings.filterwarnings("ignore")
 
-user = '--'
-pwd = '--'
-broker = "--"
 topic = 'PI7'
 
 client = paho.Client() 
-print("Connecting to broker ",broker)
-client.username_pw_set(user, password=pwd)
-client.connect(broker, 1883)
+print("Connecting to broker ",config.broker)
+client.username_pw_set(config.user, password=config.pwd)
+client.connect(config.broker, 1883)
 
 # API Secret Key
-owm = OWM('--')
+owm = OWM(config.owm_key)
 mgr = owm.weather_manager()
 
 while True:
     observation = mgr.weather_at_place('Pelotas,BR')
     w = observation.weather
     
-    temp = {"temp": w.temperature('celsius')['temp'], "date": observation.reception_time(timeformat='iso')}
+    # temp = {"temp": w.temperature('celsius')['temp'], "date": observation.reception_time(timeformat='iso')}
     
-    # temp = w.temperature('celsius')['temp']
+    temp = w.temperature('celsius')['temp']
     obj = json.dumps({"id": "miguel-db-temp", "data": "%s"%temp})
 
     print("Publishing...", obj)
